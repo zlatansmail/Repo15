1bb949e39d23e3cae492601992fd8c35d520c02c
zlatansmail <zlatan.s@outlook.com>
2021-09-28 01:17:55 +0200

    redone ex04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -2,6 +2,15 @@ const milili = [10, 25, 4];
 
 function myArr(mili) {
     'use strict';
+    mili = [];
+    mili.push(25);
+    mili.unshift(10);
+    mili.unshift(4);
 
-    mili =
+
+    return mili;
 }
+
+console.log(myArr(milili));
+
+module.exports = myArr;
